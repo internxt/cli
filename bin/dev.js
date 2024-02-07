5f3a523611939567c51b7f89ef1bc7c31e070a47
Pixo <robertogarciadevelopment@gmail.com>
2024-02-07 17:52:39 +0100

    [PB-1328]:(fix) Setup config service with env variables

--- bin/dev.js
+++ bin/dev.js
@@ -1,9 +1,7 @@
-#!/usr/bin/env -S node --loader ts-node/esm --no-warnings=ExperimentalWarning
-
-// eslint-disable-next-line node/shebang
-async function main() {
-  const { execute } = await import('@oclif/core');
-  await execute({ development: true, dir: import.meta.url });
-}
-
-await main();
+#!/usr/bin/env node_modules/.bin/ts-node
+/* eslint-disable */
+require('dotenv').config();
+(async () => {
+  const oclif = await import('@oclif/core');
+  await oclif.execute({ development: true, dir: __dirname });
+})();
