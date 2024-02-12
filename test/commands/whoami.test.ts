fc17e9a9957745c9d6c4b52d1309a00c5b8b5b48
larry-internxt <larry@internxt.com>
2024-02-12 14:09:12 +0100

    added missing type to whoami test

--- test/commands/whoami.test.ts
+++ test/commands/whoami.test.ts
@@ -1,10 +1,11 @@
 import { expect, test } from '@oclif/test';
+import { OclifStdoutContext } from '../types/oclif-test.types';
 
 describe('whoami', () => {
   test
     .stdout()
     .command(['whoami'])
-    .it('runs whoami', (ctx) => {
+    .it('runs whoami', (ctx: OclifStdoutContext) => {
       expect(ctx.stdout).to.contain('You are');
     });
 });
