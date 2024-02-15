7014b10d990c3ae33e2c3e92ffef6206a418b1e3
larry-internxt <larry@internxt.com>
2024-02-15 17:12:30 +0100

    added a disabled cryptoJS test

--- test/services/crypto.service.test.ts
+++ test/services/crypto.service.test.ts
@@ -117,4 +117,42 @@ describe('Crypto service', () => {
     expect(expectedKeys).to.be.eql(resultedKeys);
     expect(kerysServiceStub).to.be.calledWith(password);
   });
+
+  it('The node:crypto works the same as CryptoJS library', () => {
+    return;
+    const password = {
+      value: crypto.randomBytes(16).toString('hex'),
+      salt: crypto.randomBytes(16).toString('hex'),
+    };
+    const APP_CRYPTO_SECRET = crypto.randomBytes(16).toString('hex');
+
+    // test PBKDF2 - node:crypto equivalent password to hash
+    const actualPass = CryptoJS.PBKDF2(password.value, password.salt, {
+      keySize: 256 / 32,
+      iterations: 10000,
+    }).toString();
+    const expectedPass = crypto.pbkdf2Sync(password.value, password.salt, 10000, 256 / 8, 'sha256').toString('hex');
+    expect(actualPass).to.equal(expectedPass);
+    expect(CryptoJS.lib.WordArray.random(128 / 8).toString().length).to.equal(
+      crypto.randomBytes(128 / 8).toString('hex').length,
+    );
+
+    // test CryptoJS.AES - node:crypto equivalent encrypt/decrypt
+    const cryptoJSEncrypted = CryptoJS.enc.Base64.parse(
+      CryptoJS.AES.encrypt(password.value, APP_CRYPTO_SECRET).toString(),
+    ).toString(CryptoJS.enc.Hex);
+    const textdecrypted = CryptoService.instance.decryptTextWithKey(cryptoJSEncrypted, APP_CRYPTO_SECRET);
+    expect(password.value).to.equal(textdecrypted);
+
+    const textencrypted = CryptoService.instance.encryptTextWithKey(password.value, APP_CRYPTO_SECRET);
+    const cryptoJSdecrypted = CryptoJS.AES.decrypt(
+      CryptoJS.enc.Hex.parse(textencrypted).toString(CryptoJS.enc.Base64),
+      APP_CRYPTO_SECRET,
+    ).toString(CryptoJS.enc.Utf8);
+    expect(password.value).to.equal(cryptoJSdecrypted);
+
+    const expectedText1 = CryptoService.instance.encryptTextWithKey(password.value, APP_CRYPTO_SECRET);
+    const expectedText2 = CryptoService.instance.decryptTextWithKey(expectedText1, APP_CRYPTO_SECRET);
+    expect(password.value).to.equal(expectedText2);
+  });
 });
