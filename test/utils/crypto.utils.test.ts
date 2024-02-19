46d2fcd4c8678b3694ad319c46fd80da35387244
Pixo <robertogarciadevelopment@gmail.com>
2024-02-19 15:53:05 +0100

    [PB-1329]:(feature) Add missing CryptoUtils test case

--- test/utils/crypto.utils.test.ts
+++ test/utils/crypto.utils.test.ts
@@ -1,7 +1,15 @@
 import { expect } from 'chai';
 import { CryptoUtils } from '../../src/utils/crypto.utils';
-
+import sinon from 'sinon';
+import { ConfigService } from '../../src/services/config.service';
+import { AesInit } from '../../src/types/keys.types';
+import crypto from 'node:crypto';
 describe('Crypto utils', () => {
+  const sandbox = sinon.createSandbox();
+  const aesInit: AesInit = {
+    iv: crypto.randomBytes(16).toString('hex'),
+    salt: crypto.randomBytes(64).toString('hex'),
+  };
   it('When Magic IV or Magic Salt are missing should throw an error', async () => {
     try {
       CryptoUtils.getAesInit();
@@ -9,5 +17,14 @@ describe('Crypto utils', () => {
     } catch {
       // Noop
     }
+  });
+
+  it('When aes information is required, then it is read from the config service', async () => {
+    const configServiceInstanceStub = sandbox.stub(ConfigService.instance, 'get');
+    configServiceInstanceStub.withArgs('APP_MAGIC_IV').returns(aesInit.iv);
+    configServiceInstanceStub.withArgs('APP_MAGIC_SALT').returns(aesInit.salt);
+
+    const result = CryptoUtils.getAesInit();
+    expect(result).to.eql(aesInit);
   });
 });
