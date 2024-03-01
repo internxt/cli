f015355d79d21bb5b530c538b77694f8edeb8cd8
larry-internxt <larry@internxt.com>
2024-03-01 10:33:26 +0100

    fixed static readonly fields

--- src/utils/format.utils.ts
+++ src/utils/format.utils.ts
@@ -1,12 +1,12 @@
 import dayjs from 'dayjs';
 
 export class FormatUtils {
-  static humanFileSize = (size: number) => {
+  static readonly humanFileSize = (size: number) => {
     const i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
     return Number((size / Math.pow(1024, i)).toFixed(2)) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
   };
 
-  static formatDate = (date: string | Date) => {
+  static readonly formatDate = (date: string | Date) => {
     return dayjs(date).format('D MMMM, YYYY [at] HH:mm');
   };
 }
