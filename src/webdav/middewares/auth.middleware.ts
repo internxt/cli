d9503ca45198a088d4d86784d16092f300fbc9b3
Pixo <robertogarciadevelopment@gmail.com>
2024-03-06 14:58:45 +0100

    [PB_1766]:(feature) Remove async from auth middleware

--- src/webdav/middewares/auth.middleware.ts
+++ src/webdav/middewares/auth.middleware.ts
@@ -3,20 +3,22 @@ import { ConfigService } from '../../services/config.service';
 import { SdkManager } from '../../services/sdk-manager.service';
 
 export const AuthMiddleware = (configService: ConfigService): RequestHandler => {
-  return async (req, res, next) => {
-    try {
-      const credentials = await configService.readUser();
-      if (!credentials) throw new Error('Unauthorized');
-      SdkManager.init({
-        token: credentials.token,
-        newToken: credentials.newToken,
-      });
-      req.user = {
-        rootFolderId: credentials.user.root_folder_id,
-      };
-      next();
-    } catch (error) {
-      res.status(401).send({ error: (error as Error).message });
-    }
+  return (req, res, next) => {
+    (async () => {
+      try {
+        const credentials = await configService.readUser();
+        if (!credentials) throw new Error('Unauthorized');
+        SdkManager.init({
+          token: credentials.token,
+          newToken: credentials.newToken,
+        });
+        req.user = {
+          rootFolderId: credentials.user.root_folder_id,
+        };
+        next();
+      } catch (error) {
+        res.status(401).send({ error: (error as Error).message });
+      }
+    })();
   };
 };
