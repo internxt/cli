2c482380c211db8a222e3bf5739ba4fc1496bd10
Pixo <robertogarciadevelopment@gmail.com>
2024-03-18 18:52:27 +0100

    [PB-1337]:(feature) Add logs command docs

--- src/commands/logs.ts
+++ src/commands/logs.ts
@@ -2,13 +2,9 @@ import { Command } from '@oclif/core';
 import { ConfigService } from '../services/config.service';
 
 export default class Logs extends Command {
-  static description = 'describe the command here';
+  static readonly description = 'Displays the Internxt CLI logs directory path';
 
   static examples = ['<%= config.bin %> <%= command.id %>'];
-
-  static flags = {};
-
-  static args = {};
 
   public async run(): Promise<void> {
     this.log(`Internxt CLI logs are at ${ConfigService.INTERNXT_CLI_LOGS_DIR}`);
