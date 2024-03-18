8278721f6abaeb3f7cdc5dce403e5b1589c11076
Pixo <robertogarciadevelopment@gmail.com>
2024-03-18 18:50:18 +0100

    [PB-1337]:(feature) Fix code quality issues

--- src/utils/pm2.utils.ts
+++ src/utils/pm2.utils.ts
@@ -29,15 +29,13 @@ export class PM2Utils {
       pm2.describe(this.WEBDAV_APP_NAME, (err, processDescription) => {
         if (err) {
           reject(err);
+        } else if (processDescription.length === 0) {
+          reject(new Error('WebDav server is not running'));
         } else {
-          if (processDescription.length === 0) {
-            reject(new Error('WebDav server is not running'));
-          } else {
-            const process = processDescription[0];
-            resolve({
-              status: (process.pm2_env?.status ?? 'unknown') as WebDavProcessStatus,
-            });
-          }
+          const process = processDescription[0];
+          resolve({
+            status: (process.pm2_env?.status ?? 'unknown') as WebDavProcessStatus,
+          });
         }
       });
     });
