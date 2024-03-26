b3ab3893eca9d3ba48a2413777f69c885fd7a38a
larry-internxt <larry@internxt.com>
2024-03-26 18:33:45 +0100

    added cli command to get the root folder id

--- src/commands/root.ts
+++ src/commands/root.ts
@@ -2,10 +2,11 @@ import { Command } from '@oclif/core';
 import { ConfigService } from '../services/config.service';
 import { CLIUtils } from '../utils/cli.utils';
 import { ErrorUtils } from '../utils/errors.utils';
+import { ValidationService } from '../services/validation.service';
 
-export default class Whoami extends Command {
+export default class Root extends Command {
   static readonly args = {};
-  static readonly description = 'Display the current user logged into the Internxt CLI.';
+  static readonly description = 'Display the current root folder id from the user logged into the Internxt CLI.';
 
   static readonly examples = ['<%= config.bin %> <%= command.id %>'];
 
@@ -19,8 +20,12 @@ export default class Whoami extends Command {
 
   public async run(): Promise<void> {
     const userCredentials = await ConfigService.instance.readUser();
-    if (userCredentials?.user?.email) {
-      CLIUtils.success(`You are logged in with: ${userCredentials.user.email}`);
+    if (
+      userCredentials?.user &&
+      userCredentials.root_folder_uuid &&
+      ValidationService.instance.validateUUIDv4(userCredentials.root_folder_uuid)
+    ) {
+      CLIUtils.success(`Your root folder id is: ${userCredentials.root_folder_uuid}`);
     } else {
       CLIUtils.error('You are not logged in');
     }
