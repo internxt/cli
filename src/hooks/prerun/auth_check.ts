4b59c7ccc201ad0c128c9cf9b61871f47a51acb9
Pixo <robertogarciadevelopment@gmail.com>
2024-04-09 15:04:55 +0200

    [_]:(fix) Fix auth check hook imports

--- src/hooks/prerun/auth_check.ts
+++ src/hooks/prerun/auth_check.ts
@@ -1,15 +1,12 @@
 import { Hook } from '@oclif/core';
-import Whoami from '../../commands/whoami';
-import Login from '../../commands/login';
-import Logout from '../../commands/logout';
 import { CLIUtils } from '../../utils/cli.utils';
 import { SdkManager } from '../../services/sdk-manager.service';
 import { AuthService } from '../../services/auth.service';
 import { MissingCredentialsError } from '../../types/command.types';
 
-const CommandsToSkip = [Whoami, Login, Logout];
+const CommandsToSkip = ['whoami', 'login', 'logout'];
 const hook: Hook<'prerun'> = async function (opts) {
-  if (!CommandsToSkip.map((command) => command.name).includes(opts.Command.name)) {
+  if (!CommandsToSkip.includes(opts.Command.name)) {
     CLIUtils.doing('Checking credentials');
     try {
       const { token, newToken } = await AuthService.instance.getAuthDetails();
