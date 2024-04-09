40679c3e2fedf7728da6df6279fc79e12603e022
larry-internxt <larry@internxt.com>
2024-04-09 16:40:52 +0200

    improved commands to skip

--- src/hooks/prerun/auth_check.ts
+++ src/hooks/prerun/auth_check.ts
@@ -1,12 +1,15 @@
 import { Hook } from '@oclif/core';
+import Whoami from '../../commands/whoami';
+import Login from '../../commands/login';
+import Logout from '../../commands/logout';
 import { CLIUtils } from '../../utils/cli.utils';
 import { SdkManager } from '../../services/sdk-manager.service';
 import { AuthService } from '../../services/auth.service';
 import { MissingCredentialsError } from '../../types/command.types';
 
-const CommandsToSkip = ['whoami', 'login', 'logout'];
+const CommandsToSkip = [Whoami, Login, Logout];
 const hook: Hook<'prerun'> = async function (opts) {
-  if (!CommandsToSkip.includes(opts.Command.name.toLowerCase())) {
+  if (!CommandsToSkip.map((command) => command.name).includes(opts.Command.name)) {
     CLIUtils.doing('Checking credentials');
     try {
       const { token, newToken } = await AuthService.instance.getAuthDetails();
