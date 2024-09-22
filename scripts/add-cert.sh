7440ecb5056566363382fa156aa648f7334778c4
Sk Razee <182448076+skrazee@users.noreply.github.com>
2024-09-23 03:27:35 +0600

    Added additional checking using elif for Fedora

--- scripts/add-cert.sh
+++ scripts/add-cert.sh
@@ -4,6 +4,9 @@ CERT_PATH=$1
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain $CERT_PATH
+elif ! [[ -d "/usr/local/share/ca-certificates/" ]]; then # For Fedora distribution
+    sudo cp $CERT_PATH /etc/pki/ca-trust/source/anchors/
+    sudo update-ca-trust
 else
     sudo cp $CERT_PATH /usr/local/share/ca-certificates/
     sudo update-ca-certificates
