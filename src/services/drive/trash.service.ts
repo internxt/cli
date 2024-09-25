705f642c1fbecad88099f1134356970003b87bb6
larry-internxt <larry@internxt.com>
2024-09-25 16:20:59 +0200

    added list trash items command

--- src/services/drive/trash.service.ts
+++ src/services/drive/trash.service.ts
@@ -1,5 +1,6 @@
-import { StorageTypes } from '@internxt/sdk/dist/drive';
+import { StorageTypes, Trash } from '@internxt/sdk/dist/drive';
 import { SdkManager } from '../sdk-manager.service';
+import { FetchPaginatedFile, FetchPaginatedFolder } from '@internxt/sdk/dist/drive/storage/types';
 
 export class TrashService {
   static readonly instance = new TrashService();
@@ -7,5 +8,34 @@ export class TrashService {
   public trashItems = (payload: StorageTypes.AddItemsToTrashPayload) => {
     const storageClient = SdkManager.instance.getStorage(true);
     return storageClient.addItemsToTrash(payload);
+  };
+
+  public getTrashFolderContent = async () => {
+    const storageClient = SdkManager.instance.getTrash();
+    const folders = await this.getAllTrashSubfolders(storageClient, 0);
+    const files = await this.getAllTrashSubfiles(storageClient, 0);
+    return { folders, files };
+  };
+
+  private getAllTrashSubfolders = async (storageClient: Trash, offset: number): Promise<FetchPaginatedFolder[]> => {
+    const folderContentPromise = storageClient.getTrashedFilesPaginated(50, offset, 'folders', true);
+    const { result: folders } = (await folderContentPromise) as unknown as { result: FetchPaginatedFolder[] };
+
+    if (folders.length > 0) {
+      return folders.concat(await this.getAllTrashSubfolders(storageClient, offset + folders.length));
+    } else {
+      return folders;
+    }
+  };
+
+  private getAllTrashSubfiles = async (storageClient: Trash, offset: number): Promise<FetchPaginatedFile[]> => {
+    const folderContentPromise = storageClient.getTrashedFilesPaginated(50, offset, 'files', true);
+    const { result: folders } = (await folderContentPromise) as unknown as { result: FetchPaginatedFile[] };
+
+    if (folders.length > 0) {
+      return folders.concat(await this.getAllTrashSubfiles(storageClient, offset + folders.length));
+    } else {
+      return folders;
+    }
   };
 }
