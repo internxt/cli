80de71a1e5af557875d754663fb8ff66aee0ad5a
larry-internxt <larry@internxt.com>
2024-10-31 13:50:23 +0100

    fixed and improved HEAD handler and tests

--- src/webdav/handlers/HEAD.handler.ts
+++ src/webdav/handlers/HEAD.handler.ts
@@ -1,8 +1,9 @@
 import { WebDavMethodHandler } from '../../types/webdav.types';
 import { Request, Response } from 'express';
+
 export class HEADRequestHandler implements WebDavMethodHandler {
-  async handle(req: Request, res: Response) {
+  handle = async (_: Request, res: Response) => {
     // This is a NOOP request handler, clients like CyberDuck uses this.
     res.status(405).send();
-  }
+  };
 }
--- test/webdav/handlers/HEAD.handler.test.ts
+++ test/webdav/handlers/HEAD.handler.test.ts
@@ -1,25 +1,26 @@
 import sinon from 'sinon';
 import { HEADRequestHandler } from '../../../src/webdav/handlers/HEAD.handler';
 import { createWebDavRequestFixture, createWebDavResponseFixture } from '../../fixtures/webdav.fixture';
+import { expect } from 'chai';
 
 describe('HEAD request handler', () => {
   const sandbox = sinon.createSandbox();
+
   afterEach(() => {
     sandbox.restore();
   });
+
   it('When a WebDav client sends a HEAD request, it should reply with a 405', async () => {
     const requestHandler = new HEADRequestHandler();
 
     const request = createWebDavRequestFixture({
       method: 'HEAD',
     });
-
     const response = createWebDavResponseFixture({
       status: sandbox.stub().returns({ send: sandbox.stub() }),
     });
 
     await requestHandler.handle(request, response);
-
-    sinon.assert.calledWith(response.status, 405);
+    expect(response.status.calledWith(405)).to.be.true;
   });
 });
