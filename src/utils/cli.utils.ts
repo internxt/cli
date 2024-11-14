fb0a3d19f29d1980394e933d8469bfcbcba3cd05
larry-internxt <larry@internxt.com>
2024-11-14 11:39:09 +0100

    improved CLIUtils functionalities

--- src/utils/cli.utils.ts
+++ src/utils/cli.utils.ts
@@ -1,30 +1,34 @@
 import { ux, Flags } from '@oclif/core';
 
 export class CLIUtils {
-  static clearPreviousLine() {
+  static readonly clearPreviousLine = () => {
     process.stdout.write('\x1b[1A');
     process.stdout.clearLine(0);
-  }
+  };
 
-  static warning(message: string) {
-    ux.log(ux.colorize('#a67805', `⚠ Warning: ${message}`));
-  }
+  static readonly warning = (reporter: (message: string) => void, message: string) => {
+    reporter(ux.colorize('#a67805', `⚠ Warning: ${message}`));
+  };
 
-  static error(message: string) {
-    ux.log(ux.colorize('red', `⚠ Error: ${message}`));
-  }
+  static readonly error = (reporter: (message: string) => void, message: string) => {
+    reporter(ux.colorize('red', `⚠ Error: ${message}`));
+  };
 
-  static doing(message: string) {
+  static readonly doing = (message: string) => {
     ux.action.start(message, undefined, {});
-  }
+  };
 
-  static success(message: string) {
-    ux.log(ux.colorize('green', `✓ ${message}`));
-  }
+  static readonly success = (reporter: (message: string) => void, message: string) => {
+    reporter(ux.colorize('green', `✓ ${message}`));
+  };
 
-  static done() {
+  static readonly log = (reporter: (message: string) => void, message: string) => {
+    reporter(`${message}`);
+  };
+
+  static readonly done = () => {
     ux.action.stop(ux.colorize('green', 'done ✓'));
-  }
+  };
 
   static readonly CommonFlags = {
     'non-interactive': Flags.boolean({
@@ -32,73 +36,87 @@ export class CLIUtils {
       env: 'INXT_NONINTERACTIVE',
       helpGroup: 'helper',
       description:
-        'Blocks the cli from being interactive. If passed, the cli will not request data through the console and will throw errors directly',
+        'Prevents the CLI from being interactive. When enabled, the CLI will not request input through the console and will throw errors directly.',
       required: false,
     }),
   };
 
-  static readonly getValueFromFlag = (
-    flag: { value?: string; name: string; error: Error; canBeEmpty?: boolean },
-    nonInteractive: boolean,
-    validate: (value: string) => boolean,
-  ): string | undefined => {
+  static readonly getValueFromFlag = async (
+    flag: {
+      value?: string;
+      name: string;
+    },
+    command: {
+      nonInteractive: boolean;
+      maxAttempts?: number;
+      prompt: { message: string; options?: ux.IPromptOptions };
+    },
+    validation: {
+      validate: (value: string) => Promise<boolean> | boolean;
+      error: Error;
+      canBeEmpty?: boolean;
+    },
+    reporter: (message: string) => void,
+  ): Promise<string> => {
     // It returns the value passed from the flag if it is valid. If it is not valid, it will throw an error when nonInteractive mode is active and a warning otherwise
     // It throws an error if nonInteractive mode is active and no flag value is aquired
-    if (flag.canBeEmpty) {
-      if (!flag.value || flag.value.trim().length === 0) {
+    if (flag.value) {
+      if (validation.canBeEmpty && flag.value.trim().length === 0) {
         return '';
       }
-    }
-    if (flag.value) {
-      const isValid = validate(flag.value);
+      const isValid = await validation.validate(flag.value);
       if (isValid) {
         return flag.value;
-      } else if (nonInteractive) {
-        throw flag.error;
       } else {
-        CLIUtils.error(flag.error.message);
+        if (command.nonInteractive) {
+          throw validation.error;
+        } else {
+          CLIUtils.error(reporter, validation.error.message);
+        }
       }
-    } else if (nonInteractive) {
+    }
+    if (command.nonInteractive) {
       throw new NoFlagProvidedError(flag.name);
+    } else {
+      return await CLIUtils.promptWithAttempts(command.prompt, command.maxAttempts, validation, reporter);
     }
   };
 
-  static readonly promptWithAttempts = async (
-    prompt: { message: string; options?: ux.IPromptOptions; error: Error },
-    maxAttempts: number,
-    validate: (value: string) => boolean,
+  private static readonly promptWithAttempts = async (
+    prompt: { message: string; options?: ux.IPromptOptions },
+    maxAttempts = 3,
+    validation: {
+      validate: (value: string) => Promise<boolean> | boolean;
+      error: Error;
+      canBeEmpty?: boolean;
+    },
+    reporter: (message: string) => void,
   ): Promise<string> => {
     let isValid = false;
     let currentAttempts = 0;
     let promptValue = '';
     do {
       promptValue = await ux.prompt(prompt.message, prompt.options);
-      isValid = validate(promptValue);
+      if (validation.canBeEmpty) {
+        if (!promptValue || promptValue.trim().length === 0) {
+          return '';
+        }
+      }
+      isValid = await validation.validate(promptValue);
       if (!isValid) {
         currentAttempts++;
         if (currentAttempts < maxAttempts) {
-          CLIUtils.warning(prompt.error.message + ', please type it again');
+          CLIUtils.warning(reporter, validation.error.message + ', please type it again');
         }
       }
     } while (!isValid && currentAttempts < maxAttempts);
 
     if (!isValid) {
-      throw prompt.error;
+      throw validation.error;
     } else {
       return promptValue;
     }
   };
-
-  static timer() {
-    const start = new Date();
-    return {
-      stop: () => {
-        const end = new Date();
-        const time = end.getTime() - start.getTime();
-        return time;
-      },
-    };
-  }
 
   static readonly prompt = async (
     prompt: { message: string; options?: ux.IPromptOptions; error?: Error },
@@ -112,6 +130,17 @@ export class CLIUtils {
       }
     }
     return promptValue;
+  };
+
+  static readonly timer = () => {
+    const start = new Date();
+    return {
+      stop: () => {
+        const end = new Date();
+        const time = end.getTime() - start.getTime();
+        return time;
+      },
+    };
   };
 
   static readonly parseEmpty = async (input: string) => (input.trim().length === 0 ? ' ' : input);
