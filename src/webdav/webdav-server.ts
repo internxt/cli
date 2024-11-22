1ca2b3cda55b78537f6e23fb4ca8f31623273503
larry-internxt <larry@internxt.com>
2024-11-22 10:33:34 +0100

    removed unused analytics

--- src/webdav/webdav-server.ts
+++ src/webdav/webdav-server.ts
@@ -29,7 +29,6 @@ import { PROPPATCHRequestHandler } from './handlers/PROPPATCH.handler';
 import { MOVERequestHandler } from './handlers/MOVE.handler';
 import { COPYRequestHandler } from './handlers/COPY.handler';
 import { TrashService } from '../services/drive/trash.service';
-import { AnalyticsService } from '../services/analytics.service';
 
 export class WebDavServer {
   constructor(
@@ -62,12 +61,9 @@ export class WebDavServer {
     this.app.use(ErrorHandlingMiddleware);
     this.app.use(AuthMiddleware(ConfigService.instance));
     this.app.use(
-      RequestLoggerMiddleware(
-        {
-          enable: true,
-        },
-        AnalyticsService.instance,
-      ),
+      RequestLoggerMiddleware({
+        enable: true,
+      }),
     );
   };
 
