c0dd84412b0ce29192d79478370d99a8f63ddce8
larry-internxt <larry@internxt.com>
2024-12-23 13:13:49 +0100

    implemented HEAD operation

--- src/webdav/handlers/HEAD.handler.ts
+++ src/webdav/handlers/HEAD.handler.ts
@@ -1,9 +1,51 @@
-import { WebDavMethodHandler } from '../../types/webdav.types';
 import { Request, Response } from 'express';
+import { WebDavMethodHandler } from '../../types/webdav.types';
+import { WebDavUtils } from '../../utils/webdav.utils';
+import { webdavLogger } from '../../utils/logger.utils';
+import { DriveFileService } from '../../services/drive/drive-file.service';
+import { DriveDatabaseManager } from '../../services/database/drive-database-manager.service';
+import { DriveFileItem } from '../../types/drive.types';
+import { NetworkUtils } from '../../utils/network.utils';
 
 export class HEADRequestHandler implements WebDavMethodHandler {
-  handle = async (_: Request, res: Response) => {
-    // This is a NOOP request handler, clients like CyberDuck uses this.
-    res.status(405).send();
+  constructor(
+    private readonly dependencies: {
+      driveFileService: DriveFileService;
+      driveDatabaseManager: DriveDatabaseManager;
+    },
+  ) {}
+
+  handle = async (req: Request, res: Response) => {
+    const { driveDatabaseManager, driveFileService } = this.dependencies;
+    const resource = await WebDavUtils.getRequestedResource(req);
+
+    if (resource.type === 'folder') {
+      res.status(200).send();
+      return;
+    }
+
+    webdavLogger.info(`[HEAD] Request received for ${resource.type} at ${resource.url}`);
+    const driveFile = (await WebDavUtils.getAndSearchItemFromResource({
+      resource,
+      driveDatabaseManager,
+      driveFileService,
+    })) as DriveFileItem;
+
+    webdavLogger.info(`[HEAD] [${driveFile.uuid}] Found Drive File`);
+
+    const range = req.headers['range'];
+    const rangeOptions = NetworkUtils.parseRangeHeader({
+      range,
+      totalFileSize: driveFile.size,
+    });
+    let contentLength = driveFile.size;
+    if (rangeOptions) {
+      webdavLogger.info(`[HEAD] [${driveFile.uuid}] Range request received:`, { rangeOptions });
+      contentLength = rangeOptions.rangeSize;
+    }
+
+    res.header('Content-Type', 'application/octet-stream');
+    res.header('Content-length', contentLength.toString());
+    res.status(200).send();
   };
 }
