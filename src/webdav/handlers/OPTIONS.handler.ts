215973b36cf87b1690f7ce5a8b5a1cc76ba9f750
larry-internxt <larry@internxt.com>
2024-12-23 13:23:04 +0100

    implemented OPTIONS operation

--- src/webdav/handlers/OPTIONS.handler.ts
+++ src/webdav/handlers/OPTIONS.handler.ts
@@ -1,10 +1,39 @@
 import { WebDavMethodHandler } from '../../types/webdav.types';
 import { Request, Response } from 'express';
+import { WebDavUtils } from '../../utils/webdav.utils';
+import { webdavLogger } from '../../utils/logger.utils';
 
 export class OPTIONSRequestHandler implements WebDavMethodHandler {
-  handle = async (_: Request, res: Response) => {
-    res.header('Allow', 'OPTIONS, GET, HEAD, POST, PUT, DELETE, PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK, UNLOCK');
-    res.header('DAV', '1, 2, ordered-collections');
-    res.status(200).send();
+  handle = async (req: Request, res: Response) => {
+    const resource = await WebDavUtils.getRequestedResource(req);
+
+    webdavLogger.info(`[OPTIONS] Request received for ${resource.type} at ${resource.url}`);
+
+    if (resource.url === '/' || resource.url === '') {
+      const allowedMethods = 'DELETE, GET, HEAD, MKCOL, MOVE, OPTIONS, PROPFIND, PUT';
+      webdavLogger.info(`[OPTIONS] Returning Allowed Options: ${allowedMethods}`);
+      res.header('Allow', 'DELETE, GET, HEAD, MKCOL, MOVE, OPTIONS, PROPFIND, PUT');
+      res.header('DAV', '1, 2, ordered-collections');
+      res.status(200).send();
+      return;
+    }
+
+    if (resource.type === 'folder') {
+      const allowedMethods = 'DELETE, HEAD, MKCOL, MOVE, OPTIONS, PROPFIND';
+      webdavLogger.info(`[OPTIONS] Returning Allowed Options: ${allowedMethods}`);
+      res.header('Allow', allowedMethods);
+      res.header('DAV', '1, 2, ordered-collections');
+      res.status(200).send();
+      return;
+    }
+
+    if (resource.type === 'file') {
+      const allowedMethods = 'DELETE, GET, HEAD, MOVE, OPTIONS, PROPFIND, PUT';
+      webdavLogger.info(`[OPTIONS] Returning Allowed Options: ${allowedMethods}`);
+      res.header('Allow', allowedMethods);
+      res.header('DAV', '1, 2, ordered-collections');
+      res.status(200).send();
+      return;
+    }
   };
 }
