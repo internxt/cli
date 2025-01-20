3fb2dc0d394c51a74e70487bbc5dc2a81a45f701
larry-internxt <larry@internxt.com>
2025-01-20 11:29:20 +0100

    refactor: replace postinstall script and add restart-webdav script

---
+++ scripts/restart-webdav.js
@@ -0,0 +1,16 @@
+const { exec } = require('child_process');
+
+async function runCommand() {
+  await new Promise((resolve, reject) => {
+    exec('node ./bin/run.js webdav restart', (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(stdout || stderr);
+    });
+  });
+}
+
+runCommand().finally(() => {
+  process.exit(0);
+});
