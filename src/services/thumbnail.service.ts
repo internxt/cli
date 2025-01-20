489657dcc3d8a66f12f2a61fbcc13ce73b5b3e22
larry-internxt <larry@internxt.com>
2025-01-20 16:22:25 +0100

    refactor(thumbnail): replaced thumbnail upload with new inxt-js functionality

--- src/services/thumbnail.service.ts
+++ src/services/thumbnail.service.ts
@@ -2,7 +2,6 @@ import { Readable } from 'node:stream';
 import { DriveFileService } from './drive/drive-file.service';
 import { StorageTypes } from '@internxt/sdk/dist/drive';
 import { NetworkFacade } from './network/network-facade.service';
-import { UploadService } from './network/upload.service';
 import { isImageThumbnailable, ThumbnailConfig } from '../utils/thumbnail.utils';
 import sharp from 'sharp';
 
@@ -13,7 +12,6 @@ export class ThumbnailService {
     fileContent: Buffer,
     fileType: string,
     userBucket: string,
-    userMnemonic: string,
     file_id: number,
     networkFacade: NetworkFacade,
   ): Promise<StorageTypes.Thumbnail | undefined> => {
@@ -23,16 +21,21 @@ export class ThumbnailService {
     }
     if (thumbnailBuffer) {
       const size = thumbnailBuffer.length;
-      const [thumbnailPromise] = await UploadService.instance.uploadFileStream(
-        Readable.from(thumbnailBuffer),
-        userBucket,
-        userMnemonic,
-        size,
-        networkFacade,
-        () => {},
-      );
 
-      const thumbnailUploadResult = await thumbnailPromise;
+      const fileId = await new Promise((resolve: (fileId: string) => void, reject) => {
+        networkFacade.uploadFile(
+          Readable.from(thumbnailBuffer),
+          size,
+          userBucket,
+          (err: Error | null, res: string | null) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(res as string);
+          },
+          () => {},
+        );
+      });
 
       const createdThumbnailFile = await DriveFileService.instance.createThumbnail({
         file_id: file_id,
@@ -41,7 +44,7 @@ export class ThumbnailService {
         type: ThumbnailConfig.Type,
         size: size,
         bucket_id: userBucket,
-        bucket_file: thumbnailUploadResult.fileId,
+        bucket_file: fileId,
         encrypt_version: StorageTypes.EncryptionVersion.Aes03,
       });
       return createdThumbnailFile;
