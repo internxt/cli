c9a74d7f5598f960e4b93c07d3f9f47847ebdc6d
larry-internxt <larryrider@users.noreply.github.com>
2025-07-02 12:59:44 +0200

    refactor: remove unused env and rename drive-url

--- src/types/config.types.ts
+++ src/types/config.types.ts
@@ -1,9 +1,7 @@
 export interface ConfigKeys {
-  readonly DRIVE_URL: string;
+  readonly DRIVE_WEB_URL: string;
   readonly DRIVE_NEW_API_URL: string;
-  readonly PAYMENTS_API_URL: string;
   readonly APP_CRYPTO_SECRET: string;
-  readonly APP_CRYPTO_SECRET2: string;
   readonly APP_MAGIC_IV: string;
   readonly APP_MAGIC_SALT: string;
   readonly NETWORK_URL: string;
