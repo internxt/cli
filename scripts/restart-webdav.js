5c5cb27bdb04189cba1f44b598d68fdce3719acc
larry-internxt <larryrider@users.noreply.github.com>
2025-07-03 11:29:57 +0200

    refactor: remove console.logs and ensure process exits cleanly

--- scripts/restart-webdav.js
+++ scripts/restart-webdav.js
@@ -1,20 +1,17 @@
 const { exec } = require('child_process');
 
 async function runCommand() {
-  try {
-    const output = await new Promise((resolve, reject) => {
-      exec('node ./bin/run.js webdav restart', (error, stdout, stderr) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        resolve(stdout || stderr);
-      });
+  await new Promise((resolve, reject) => {
+    exec('node ./bin/run.js webdav restart', (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(stdout ?? stderr);
+      }
     });
-    console.log('Command output:', output);
-  } catch (error) {
-    console.error('Command failed with error:', error);
-  }
+  });
 }
 
-runCommand();
+runCommand().finally(() => {
+  process.exit(0);
+});
