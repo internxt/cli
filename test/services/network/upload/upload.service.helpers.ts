2a801909d30475e4a3284112e414538f917a3720
AlexisMora <alexis@internxt.com>
2025-11-27 12:01:00 +0100

    delete unused fixture

--- test/services/network/upload/upload.service.helpers.ts
+++ test/services/network/upload/upload.service.helpers.ts
@@ -37,9 +37,3 @@ export function createMockReadStream() {
   stream.push(null);
   return stream;
 }
-
-export function createMockTimer() {
-  return {
-    stop: vi.fn().mockReturnValue(1000),
-  };
-}
