eec88ecff641eff9e990334721a2dbb30844b1d5
larryrider <larryrider@users.noreply.github.com>
2026-02-18 15:22:42 +0100

    feat: init database service

--- src/webdav/index.ts
+++ src/webdav/index.ts
@@ -5,6 +5,7 @@ import { ConfigService } from '../services/config.service';
 import { AuthService } from '../services/auth.service';
 import { webdavLogger } from '../utils/logger.utils';
 import { SdkManager } from '../services/sdk-manager.service';
+import { DatabaseService } from '../services/database/database.service';
 
 dotenv.config({ quiet: true });
 
@@ -12,6 +13,8 @@ const init = async () => {
   await ConfigService.instance.ensureInternxtCliDataDirExists();
   await ConfigService.instance.ensureWebdavCertsDirExists();
   await ConfigService.instance.ensureInternxtLogsDirExists();
+
+  await DatabaseService.instance.initialize();
 
   const { token, workspace } = await AuthService.instance.getAuthDetails();
   SdkManager.init({ token, workspaceToken: workspace?.workspaceCredentials.token });
