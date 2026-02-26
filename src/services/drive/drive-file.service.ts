de10db131c8117ae4a4e34d2f13827f5b15beb46
larryrider <larryrider@users.noreply.github.com>
2026-02-26 20:23:32 +0100

    feat: implement getByParentUuidAndName method and enhance getFileMetadataByPath

--- src/services/drive/drive-file.service.ts
+++ src/services/drive/drive-file.service.ts
@@ -4,6 +4,10 @@ import { DriveFileItem } from '../../types/drive.types';
 import { DriveUtils } from '../../utils/drive.utils';
 import { AuthService } from '../auth.service';
 import { FileRepository } from '../database/drive-file/drive-file.repository';
+import { DriveFolderService } from './drive-folder.service';
+import { NotFoundError } from '../../utils/errors.utils';
+import { PathUtils } from '../../utils/path.utils';
+import { logger } from '../../utils/logger.utils';
 
 export class DriveFileService {
   static readonly instance = new DriveFileService();
@@ -89,10 +93,39 @@ export class DriveFileService {
     FileRepository.instance.updateByUuid(fileUuid, { name: payload.plainName, type: payload.type });
   };
 
+  public getByParentUuidAndName = async (
+    parentUuid: string,
+    name: string,
+    type: string | null,
+  ): Promise<DriveFileItem> => {
+    const subFiles = await DriveFolderService.instance.getFolderSubfiles(parentUuid);
+    const fileMeta = subFiles.find(
+      (file) => (file.plainName === name || file.name === name) && (file.type ?? null) === type,
+    );
+    if (!fileMeta) {
+      throw new NotFoundError('File not found');
+    }
+    return DriveUtils.driveFileMetaToItem(fileMeta);
+  };
+
   public getFileMetadataByPath = async (path: string): Promise<DriveFileItem> => {
-    const storageClient = SdkManager.instance.getStorage();
-    const fileMetadata = await storageClient.getFileByPath(encodeURIComponent(path));
-    return DriveUtils.driveFileMetaToItem(fileMetadata);
+    const { fileName, fileType, folderPath } = PathUtils.getPathFileData(path);
+
+    const parentFolder = await DriveFolderService.instance.getFolderMetadataByPath(folderPath);
+
+    const localFileDB = await FileRepository.instance.getByParentUuidNameAndType(parentFolder.uuid, fileName, fileType);
+    if (localFileDB) {
+      try {
+        const file = await this.getFileMetadata(localFileDB.uuid);
+        if (file) {
+          return file;
+        }
+      } catch {
+        logger.error('File not found when getting file by path on local DB', { path });
+      }
+    }
+
+    return this.getByParentUuidAndName(parentFolder.uuid, fileName, fileType);
   };
 
   public createThumbnail = (payload: StorageTypes.CreateThumbnailEntryPayload): Promise<StorageTypes.Thumbnail> => {
