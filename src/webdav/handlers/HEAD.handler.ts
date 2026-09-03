2025919c113636afb1894d1b3000f52835cfb7f1
larryrider <larryrider@users.noreply.github.com>
2026-09-03 12:11:20 +0200

    fix(HEAD): enhance range request handling and add Content-Range header support

--- src/webdav/handlers/HEAD.handler.ts
+++ src/webdav/handlers/HEAD.handler.ts
@@ -3,7 +3,7 @@ import { WebDavMethodHandler } from '../../types/webdav.types';
 import { WebDavUtils } from '../../utils/webdav.utils';
 import { webdavLogger } from '../../utils/logger.utils';
 import { NetworkUtils } from '../../utils/network.utils';
-import { NotFoundError } from '../../utils/errors.utils';
+import { NotFoundError, RangeNotSatisfiableError } from '../../utils/errors.utils';
 
 export class HEADRequestHandler implements WebDavMethodHandler {
   handle = async (req: Request, res: Response) => {
@@ -21,23 +21,32 @@ export class HEADRequestHandler implements WebDavMethodHandler {
 
     res.header('ETag', WebDavUtils.getItemETag(driveItem));
 
+    let rangeOptions;
     if (driveItem.itemType === 'file') {
       const range = req.headers['range'];
-      const rangeOptions = NetworkUtils.parseRangeHeader({
-        range,
-        totalFileSize: driveItem.size,
-      });
-      let contentLength = driveItem.size;
-      if (rangeOptions) {
-        webdavLogger.info(`[HEAD] [${driveItem.uuid}] Range request received:`, { rangeOptions });
-        contentLength = rangeOptions.rangeSize;
+      try {
+        rangeOptions = NetworkUtils.parseRangeHeader({
+          range,
+          totalFileSize: driveItem.size,
+        });
+      } catch (error) {
+        if (error instanceof RangeNotSatisfiableError) {
+          res.header('Content-Range', `bytes */${driveItem.size}`);
+        }
+        throw error;
       }
+      let contentLength = driveItem.size;
 
       res.header('Content-Type', 'application/octet-stream');
       res.header('Accept-Ranges', 'bytes');
+      if (rangeOptions) {
+        webdavLogger.info(`[HEAD] [${driveItem.uuid}] Range request received:`, { rangeOptions });
+        contentLength = rangeOptions.rangeSize;
+        res.header('Content-Range', `bytes ${rangeOptions.parsed.start}-${rangeOptions.parsed.end}/${driveItem.size}`);
+      }
       res.header('Content-length', contentLength.toString());
     }
 
-    res.status(200).send();
+    res.status(rangeOptions ? 206 : 200).send();
   };
 }
